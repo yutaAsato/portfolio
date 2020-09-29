7c8f0ee1d5cdedc1eeded98fafcd851e315de158
Yuta Asato <yuta@Yutas-MacBook-Pro.local>
2020-09-29 16:36:27 +0900

    changed homepage url

---
+++ build/precache-manifest.0f6d761cb811d75c8eea0e65fc1bb5ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd62cc17f3ad3347e5ef71cc69ed190a",
+    "url": "/yutaAsato/portfolio/index.html"
+  },
+  {
+    "revision": "95a89cb5ccecd484e83b",
+    "url": "/yutaAsato/portfolio/static/css/main.1d0445cc.chunk.css"
+  },
+  {
+    "revision": "46686886df355c05dbe7",
+    "url": "/yutaAsato/portfolio/static/js/2.622eee44.chunk.js"
+  },
+  {
+    "revision": "95a89cb5ccecd484e83b",
+    "url": "/yutaAsato/portfolio/static/js/main.1ae5c1b6.chunk.js"
+  },
+  {
+    "revision": "2d5381e496fa82596254",
+    "url": "/yutaAsato/portfolio/static/js/runtime-main.c69d4812.js"
+  },
+  {
+    "revision": "a7aadf3337b13c1d1811e42a8f2b6c66",
+    "url": "/yutaAsato/portfolio/static/media/austin.a7aadf33.webp"
+  },
+  {
+    "revision": "209dcd10075f7628d6cb393e72e4939c",
+    "url": "/yutaAsato/portfolio/static/media/beijing.209dcd10.webp"
+  },
+  {
+    "revision": "2d5a564296bffe89968809d1f076aee4",
+    "url": "/yutaAsato/portfolio/static/media/city1.2d5a5642.jpg"
+  },
+  {
+    "revision": "2bc381878816686be22cef7037544540",
+    "url": "/yutaAsato/portfolio/static/media/dallas.2bc38187.webp"
+  },
+  {
+    "revision": "b74bf8653baf74587b4d89274436728c",
+    "url": "/yutaAsato/portfolio/static/media/london.b74bf865.jpg"
+  },
+  {
+    "revision": "ebb8196532e1924ad86530b2cde4c220",
+    "url": "/yutaAsato/portfolio/static/media/newyork.ebb81965.webp"
+  }
+]);
